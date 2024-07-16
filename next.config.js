cd8c9c8880b71154d0ff7691663b2225092857d8
Wellington <wellington-158@hotmail.com>
2024-07-15 23:45:05 -0300

    ci: deploy webpage

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const isProd = process.env.NODE_ENV === "production"
+
+const nextConfig = {
+  output: "export", 
+  reactStrictMode: true,
+  basePath: isProd ? "/ignite-redux-zustand" : "",
+}
 
 module.exports = nextConfig
